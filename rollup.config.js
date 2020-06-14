b3c96c078378454b93bbcdeffca3a066e7488858
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-06-14 17:30:44 +0700

    Treat builtin node modules as external

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,25 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import builtins from 'builtin-modules';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
 import pkg from './package.json';
 
+const externals = [
+  ...builtins,
+  Object.keys(pkg.dependencies),
+];
+
+function external(id) {
+  return externals.some(ext => (id + '/').startsWith(ext + '/'));
+}
+
 export default {
+  input: {
+    hatsy: './src/index.ts',
+    'hatsy.cli': './src/cli/main.ts',
+  },
   plugins: [
     commonjs(),
     ts({
@@ -17,11 +31,7 @@ export default {
     nodeResolve(),
     sourcemaps(),
   ],
-  input: {
-    hatsy: './src/index.ts',
-    'hatsy.cli': './src/cli/main.ts',
-  },
-  external: Object.keys(pkg.dependencies),
+  external,
   output: [
     {
       format: 'cjs',
