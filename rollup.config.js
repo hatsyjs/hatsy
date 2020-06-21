d2115f2776851dd12daa03806f13c9fdd7f6e578
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-06-21 12:54:13 +0700

    Utilize `@proc7ts/rollup-helpers`

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,10 @@
+import { externalModules } from '@proc7ts/rollup-helpers';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import builtins from 'builtin-modules';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
 import pkg from './package.json';
-
-const externals = [
-  ...builtins,
-  Object.keys(pkg.dependencies),
-];
-
-function external(id) {
-  return externals.some(ext => (id + '/').startsWith(ext + '/'));
-}
 
 export default {
   input: './src/index.ts',
@@ -28,7 +19,7 @@ export default {
     nodeResolve(),
     sourcemaps(),
   ],
-  external,
+  external: externalModules(),
   output: [
     {
       file: pkg.main,
