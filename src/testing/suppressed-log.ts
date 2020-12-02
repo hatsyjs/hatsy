5574dcddd140257cae273800bb91ad3a2ec6cb2e
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-12-02 10:05:16 +0700

    Upgrade deps

--- src/testing/suppressed-log.ts
+++ src/testing/suppressed-log.ts
@@ -3,7 +3,7 @@
  * @module @hatsy/hatsy/testing
  */
 import { noop } from '@proc7ts/primitives';
-import type { RequestLogger } from '../core/logging';
+import type { RequestLogger } from '../core';
 
 /**
  * Console instance that logs nothing.
